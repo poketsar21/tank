d0d0921bebe3874c811ab38ac257f6ae19a54bd6
poketsar21 <jrhodes2202@gmail.com>
2021-12-13 12:31:00 +0000

    morning

---
+++ tank-wars-2021-stub/tank-wars-2021-stub/tank-wars-2021/js/BaseTank.js
@@ -0,0 +1,24 @@
+class BaseTank{
+    /** @type {number}*/
+    tankspeed = 100
+    /** @type {number} */
+    damageMax = 2
+    constructor(Scene,x,y, texture, frame){
+
+    }
+    update(){
+
+    }
+    damage(){
+
+    }
+    setBullets(){
+
+    }
+    burn(){
+
+    }
+    destroyed(){
+
+    }
+}
--- tank-wars-2021-stub/tank-wars-2021-stub/tank-wars-2021/js/TankScene.js
+++ tank-wars-2021-stub/tank-wars-2021-stub/tank-wars-2021/js/TankScene.js
@@ -1,11 +1,26 @@
+
 class TankScene extends Phaser.Scene {
+    /** @type {Phaser.Tilemaps.Tilemap} */
+    map
+    /** @type {Phaser.Tilemaps.TilemapLayer*/
+    destructLayer
     preload() {
+        this.load.image('Tileset', 'assets/tanks/landscape-tileset.png')
+        this.load.tilemapTiledJSON('level1', 'assets/level1.json')
 
     }
+    
     create() {
+        this.map = this.make.tilemap({key: 'level1'})
+        const landscape = this.map.addTilesetImage('landscape-tileset', 'Tileset')
+        this.map.createLayer('groundLayer',[landscape], 0, 0)
+        this.destructLayer = this.map.createLayer('destructableLayer', [landscape], 0, 0)
+        this.destructLayer.setCollisionByProperty({collide:true})
+        this.cameras.main.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels)
+        this.physics.world.setBounds(0,0, this.map.widthInPixels, this.map.heightInPixels)
 
     }
     update(time, delta) {
-
+        
     }
 }
